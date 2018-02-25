1521f5b41148afe430a60c8d70c76e256feb06db
Mark <poussard.mark@gmail.com>
2018-02-25 17:43:54 +0000

    First commit - started work on game

---
+++ js/main.js
@@ -0,0 +1,169 @@
+function Board(){
+    this.hFences = [];
+    this.vFences = [];
+    this.lots = [];
+
+    this.sizeX = 0;
+    this.sizeY = 0;
+
+    this.players = [];
+    this.currentPlyr = 0;
+    this.nbPlayers = 0;
+
+    this.initBoard(htmlBoard, sizeX, sizeY, nbPlayers){
+	this.sizeX = sizeX;
+	this.sizeY = sizeY;
+	this.htmlBoard = htmlBoard;
+	this.hFences = this.createHFences(sizeX, sizeY);
+	this.vFences = this.createVFences(sizeX, sizeY);
+	this.lots = this.createLots(sizeX, sizeY);
+    }
+
+    this.createHFences = function(sizeX, sizeY){
+	var hFences = [];
+	var j;
+	var i;
+	for(j=o; j < sizeY+1; j++){
+	    hFences[j] = [];
+	    for(i=0; i < sizeX; i++){
+		let htmlHFence = this.createHtmlHFence();
+		hFences[j].push(new HFence(htmlHFence));
+	    }
+	}
+	return hFences;
+    }
+
+    this.createHtmlHFence = function(){
+	var hFence = document.createElement("div");
+	this.htmlBoard.appendChild(hFence);
+	return hFence;
+    }
+
+    this.createVFences = function(sizeX, sizeY){
+	var vFences = [];
+	var j;
+	var i;
+	for(j=o; j < sizeY; j++){
+	    vFences[j] = [];
+	    for(i=0; i < sizeX+1; i++){
+		let htmlVFence = this.createHtmlVFence();
+		vFences[j].push(new VFence(htmlVFence));
+	    }
+	}
+	return vFences;
+    }
+
+    this.createHtmlVFence = function(){
+	var vFence = document.createElement("div");
+	this.htmlBoard.appendChild(vFence);
+	return vFence;
+    }
+
+    this.createLots = function(sizeX, sizeY){
+	var lots = [];
+	var j;
+	for(j=0; j < sizeY; j++){
+	    lots.push(createLotLine(sizeX, j));
+	}
+	return lots;
+    }
+
+    this.createLotLine = function(sizeX, j){
+	var topFences = this.hFences[j];
+	var bottomFences = this.hFences[j+1];
+	var lineFences = this.vFences[j];
+	var lineLots = [];
+	var i;
+	for(i=0; i < sizeX; i++){
+	    let topFence = topFences[i];
+	    let bottomFence = bottomFences[i];
+	    let leftFence = lineFences[i];
+	    let rightFence = lineFences[i+1];
+	    lineLots.push(this.createLot(topFence, bottomFence, leftFence, rightFence));
+	}
+	return lineLots;
+    }
+
+    this.createLot = function(topFence, bottomFence, leftFence, rightFence){
+	var htmlLot = this.createHtmlLot();
+	return new Lot(htmlLot, topFence, bottomFence, leftFence, rightFence);
+    }
+
+    this.createHtmlLot = function(){
+	var htmlLot = document.createElement("div");
+	this.htmlBoard.appendChild(htmlLot);
+	return htmlLot;
+    }
+
+    this.updateGameState = function(player){
+	this.incrementPlyr(); 
+	var sizeX = this.sizeX;
+	var sizeY = this.sizeY;
+	var j;
+	var i;
+	for(j=0; j<sizeY; j++){
+	    for(i=0; i<sizeX; i++){
+		let lot = this.lots[j][i];
+		if(lot.updateLotOwner(player)){
+		    this.currentPlyr = player.id;
+		}
+	    }
+	}
+    }
+
+    this.incrementPlyr = function(){
+	var plyr = this.currentPlyr;
+	plyr += 1;
+	plyr = plyr % this.nbPlayers;
+	this.currentPlyr = plyr;
+    }
+}
+
+function Lot(htmlObj, topFence, bottomFence, leftFence, rightFence){
+    this.htmlObj = htmlObj;
+    this.topFence = topFence;
+    this.bottomFence = bottomFence;
+    this.leftFence = leftFence;
+    this.rightFence = rightFence;
+    this.ownedBy = null;
+
+    this.isLotClosed = function(){
+	return this.topFence.isDrawn && this.bottomFence.isDrawn && this.leftFence.isDrawn && this.rightFence.isDrawn;
+    }
+
+    this.updateLotOwner = function(player){
+	if(this.ownedBy === null && this.isLotClosed() === true){
+	    this.ownedBy = player;
+	    player.points += 1;
+	    return true;
+	}
+	return false;
+    }
+}
+
+function Fence(htmlObj){
+    this.htmlObj = htmlObj;
+    this.isdrawn = false;
+
+    this.draw = function(player){
+	this.isDrawn = true;
+	board.updateGameState(player);
+    }
+
+    htmlObj.onclick = function(){
+	
+    }
+}
+
+function HFence(htmlObj){
+    Fence.call(this, htmlObj);
+}
+
+function VFence(htmlObj){
+    Fence.call(this, htmlObj);
+}
+
+function Player(id){
+    this.id = id;
+    this.points = 0;
+}
